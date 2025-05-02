c45e2739e54cd44327bc32648e090d0f6893c65c
Yokai <samadhimadushanka@gmail.com>
2025-05-02 15:05:16 +0530

    clean the template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-    <ul>
-        <learning-resource 
-        v-for="res in storedResources"
-        :key="res.id"
-        :title="res.title"
-        :description="res.description"
-        :link="res.link"
-
-        ></learning-resource>
-    </ul>
+  <stored-resources :resources="storedResources">
+  </stored-resources>
 </template>
    
 
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue'
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 
 export default {
     components:{
-        LearningResource
+        StoredResources,
     },
     data () {
         return {
             storedResources: [
                 {
                     id:'offcicial-guide',
-                    title: 'official Guide',
+                    title: 'Official Guide',
                     description: 'The Official Vue.js documentation.',
                     link: 'https://vuejs.org'
                 },
@@ -46,3 +38,19 @@ export default {
 
 
 </script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+    * {
+    box-sizing: border-box;
+    }
+
+    html {
+    font-family: 'Roboto', sans-serif;
+    }
+
+    body {
+    margin: 0;
+    }
+</style>
