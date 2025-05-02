3e9d58693fbf570a1aacbaaf64c28fbee36b464b
Yokai <samadhimadushanka@gmail.com>
2025-05-02 14:48:43 +0530

    added new 2 component and some data

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <ul>
+        <learning-resource 
+        v-for="res in storedResources"
+        :key="res.id"
+        :title="res.title"
+        :description="res.description"
+        :link="res.link"
+
+        ></learning-resource>
+    </ul>
+</template>
+   
+
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
+
+export default {
+    components:{
+        LearningResource
+    },
+    data () {
+        return {
+            storedResources: [
+                {
+                    id:'offcicial-guide',
+                    title: 'official Guide',
+                    description: 'The Official Vue.js documentation.',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id:'google',
+                    title: 'Google',
+                    description: 'Learn to google.',
+                    link: 'https://google.org'
+                },
+
+            ]
+        };
+
+    }
+}
+
+
+
+</script>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,23 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ title }}</h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p>{{ description }}</p>
+        <nav>
+            <a :href="link">View Resourece</a>
+        </nav>
+    </li>
+</template>
+
+
+<script>
+export default{
+    props: ['title', 'description', 'link']
+}
+    
+
+</script>
