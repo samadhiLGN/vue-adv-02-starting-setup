2648f5c60d83ac16f3804c0153c536584898b10f
Yokai <samadhimadushanka@gmail.com>
2025-05-04 13:39:00 +0530

    button update

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,24 @@
+<template>
+    <base-card>
+        <base-button @click="setSelectedTab('stored-resources')">Sotored Resources</base-button>
+        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+    </base-card>
+</template>
+
+
+<script>
+
+
+export default {
+    data(){
+        return {
+            selectedTab: 'stored-resources'
+        };
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
